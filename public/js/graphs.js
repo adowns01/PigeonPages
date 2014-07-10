616b3dddbe09fcf2a4c52f1bcd70b65fcbd2f167
Amelia Downs <downs.amelia@gmail.com>
2014-07-09 17:33:20 -0700

    refactored js, highcharts stuff still ugly

---
+++ public/js/graphs.js
@@ -0,0 +1,199 @@
+$(document).ready(function () {
+  getPublicationYearData();
+  getEbookData();
+  getAuthorData();
+});
+
+
+var getAuthorData = function(){
+  var ajaxRequest = $.ajax({
+    url: '/popular_authors',
+    type: 'GET'
+  })
+  ajaxRequest.success(function(data){
+    console.log(data)
+    graphAuthorData(data)
+  })
+}
+
+var getPublicationYearData = function(){
+  var ajaxRequest = $.ajax({
+    url: '/publication_year',
+    type: 'GET'
+  })
+  ajaxRequest.success(function(data){
+    graphPublicationYearData(data);
+  })
+}
+
+
+var getEbookData = function(){
+  var ajaxRequest = $.ajax({
+    url: '/ebooks',
+    type: 'GET'
+  })
+  ajaxRequest.success(function(data){
+    ebook_data = [['ebook', data.ebook], ['paper', data.paper]]
+    graphEbookData(ebook_data)
+  })
+}
+
+
+/////// BELOW IS HIGHCHARTS STUFF: READERS BEWARE ///////////////////////
+
+var graphAuthorData = function(data){
+  $('#popular_authors').highcharts({
+    chart: {
+      type: 'column'
+    },
+    colors: ["#ff8e4e", "#ff9d4e", "#ffac4e", "#ffbb4e", "#ffc94e"],
+
+    title: {
+      text: 'Number of Books Read by Most Popular Authors'
+    },
+    xAxis: {
+      type: 'category',
+      labels: {
+        rotation: -45,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Verdana, sans-serif'
+        }
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Number of Books'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    tooltip: {
+      pointFormat: '<b>{point.y:.1f} books</b>',
+    },
+    series: [{
+      name: 'Population',
+      data: data,
+      dataLabels: {
+        enabled: true,
+        rotation: -90,
+        color: '#FFFFFF',
+        align: 'right',
+        x: 4,
+        y: 10,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Verdana, sans-serif',
+          textShadow: '0 0 3px black'
+        }
+      }
+    }]
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var graphPublicationYearData = function(data){
+
+  $('#publication_year').highcharts({
+    chart: {
+      type: 'column'
+    },
+    colors: ["#ff8e4e", "#ff9d4e", "#ffac4e", "#ffbb4e", "#ffc94e"],
+
+    title: {
+      text: 'Number of Books Read by Publication Year'
+    },
+    xAxis: {
+      type: 'category',
+      labels: {
+        rotation: -45,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Verdana, sans-serif'
+        }
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Number of Books'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    tooltip: {
+      pointFormat: '<b>{point.y:.1f} books</b>',
+    },
+    series: [{
+      name: 'Population',
+      data: data,
+      dataLabels: {
+        enabled: true,
+        rotation: -90,
+        color: '#FFFFFF',
+        align: 'right',
+        x: 4,
+        y: 10,
+        style: {
+          fontSize: '13px',
+          fontFamily: 'Verdana, sans-serif',
+          textShadow: '0 0 3px black'
+        }
+      }
+    }]
+  });
+}
+
+
+
+
+
+var graphEbookData= function(data){
+
+  $('#container').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+            plotBorderWidth: 0,//null,
+            plotShadow: false
+          },
+          colors: ["#ff8e4e", "#ff9d4e", "#ffac4e", "#ffbb4e", "#ffc94e"],
+          title: {
+            text: ''
+          },
+          tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+          },
+          plotOptions: {
+            pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                  color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+              }
+            }
+          },
+          series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: data
+          }]
+        });
+
+}
